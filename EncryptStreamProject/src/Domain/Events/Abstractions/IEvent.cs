7efa6bc5a1b42a9a36529174cb88c7d6cf88407c
Luan Mello <luan.dev.mello@gmail.com>
2024-05-26 23:36:21 -0300

    feat: add database docker and config in system

---
+++ EncryptStreamProject/src/Domain/Events/Abstractions/IEvent.cs
@@ -0,0 +1,10 @@
+namespace Domain.Events;
+
+public interface IEvent
+{
+    public string Id { get; init; }
+    public string TraceKey { get; init; }
+    public string ApplicationId { get; init; }
+    public DateTimeOffset CreatedAt { get; init; }
+    public ChannelType ChannelName { get; init; }
+}
--- EncryptStreamProject/src/Domain/Events/Event.cs
+++ EncryptStreamProject/src/Domain/Events/Event.cs
@@ -1,10 +1,6 @@
 namespace Domain.Events;
 
-public abstract class Event
+public class Event
 {
-    public string Id { get; init; }
-    public string TraceKey { get; init; }
-    public string ApplicationId { get; init; }
-    public DateTimeOffset CreatedAt { get; init; }
-    public ChannelType ChannelName { get; init; }
+    
 }
