38e11b2abdc8c8e4eae2635d3a9331d32c423d4c
Luan Mello <luan.dev.mello@gmail.com>
2024-08-08 22:13:46 -0300

    chore: create UnitOfWork interface

--- EncryptStreamProject/src/Domain/Repositories/ISecretRepository.cs
+++ EncryptStreamProject/src/Domain/Repositories/ISecretRepository.cs
@@ -2,13 +2,11 @@ using Domain.Model;
 
 namespace Domain.Repositories;
 
-public interface ISecretRepository
+public interface ISecretRepository : IUnitOfWork
 {
     Task AddAsync(Secret secret);
 
     Task<Secret> GetById(string secretId);
 
     void Update(Secret secret);
-
-    Task Commit();
 }
